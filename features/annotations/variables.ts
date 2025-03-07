48ebeb414d5ed4981568052330d4ef1bf38a36f7
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-07 13:55:28 -0500

    3-15 Annotations with Variables

---
+++ features/annotations/variables.ts
@@ -0,0 +1,11 @@
+let apples: number = 5;
+
+//apples = "hello";
+let speed: string = "fast";
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+//built in objects
+let now: Date = new Date();
+//Array
+let colors: string[] = ["red", "green", "blue"];
